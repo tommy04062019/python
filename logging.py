bfd0c854dd4ba567909d4e89c0ab0c232ab76af3
tommy04062019 <btm12886@hotmail.com>
2021-10-03 11:45:57 +0700

    Create logging.py

---
+++ logging.py
@@ -0,0 +1,9 @@
+https://github.com/Delgan/loguru
+```
+from loguru import logger
+logger.add("app.log",format="",rotation="50 MB")
+logger.info("Hello")
+logger.error("Hello")
+logger.debug("Hello")
+logger.warning("Hello"
+```
