e5500dff46d275807695f80840970ca05407d591
tommy04062019 <btm12886@hotmail.com>
2021-11-08 20:12:33 +0700

    Create generator_password.py

---
+++ generator_password.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def get_random_string(length):
+    # With combination of lower and upper case
+    result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
+    # print random string
+    print(result_str)
