49148fce7206379c66afe97f18f3d53d03d96472
tommy04062019 <btm12886@hotmail.com>
2023-03-31 15:19:17 +0700

    Create range_function.py

---
+++ range_function.py
@@ -0,0 +1,8 @@
+“The range function can be used to easily generate a large list of numbers. It is used to auto-populate sequences of numbers in a list.
+The range function is simple to use. We can use it by just specifying the number of elements we want in the list. By default, it starts from zero and increments by one:
+>>> x = range(6)>>> x[0,1,2,3,4,5]
+We can also specify the end number and the step:
+>>> oddNum = range(3,29,2)
+>>> oddNum
+[3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27]
+The preceding range function will give us odd numbers starting from 3 to 29.”
